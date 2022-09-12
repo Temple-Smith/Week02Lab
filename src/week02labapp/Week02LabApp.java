dc25ae09245702075c8889045c4788a4b3e44953
Drew- <Drew-@MSI>
2022-09-12 12:58:45 -0600

    Week 2 Lab Java Application Practicing lab submission...

---
+++ src/week02labapp/Week02LabApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02labapp;
+
+/**
+ *
+ * @author Drew-
+ */
+public class Week02LabApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Week 2 Lab...");
+        // TODO code application logic here
+    }
+    
+}
